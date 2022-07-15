0e4513ac1f0fa52843864373e1bba1d61b20f87d
raphaelsack <i20029@hb.dhbw-stuttgart.de>
2022-07-15 17:34:56 +0200

    debug

--- src/Bot.ts
+++ src/Bot.ts
@@ -16,6 +16,7 @@ if (!config) {
     console.error("Failed to load config");
     process.exit(1);
 }
+console.log("Config loaded",config);
 
 console.log("Bot is starting...");
 const client = new Client({
@@ -28,49 +29,6 @@ initListeners(client);
 client.login(config.discord.token);
 
 export let dualis = new Dualis(config.dualis.user, config.dualis.password);
-
-let spC = new StundenplanCanvas({
-    freitag: [
-        {
-            moduleName: 'Rechnerarchitekturen',
-            from: '08:30',
-            to: '12:15',
-            room: 'HOR-120'
-        },
-        {
-            moduleName: 'Betriebssysteme',
-            from: '13:00',
-            to: '16:30',
-            room: 'HOR-120'
-        }
-    ],
-    dienstag: [
-        {
-            moduleName: 'Netztechnik',
-            from: '09:00',
-            to: '12:00',
-            room: 'HOR-120'
-        }
-    ],
-    donnerstag: [
-        {
-            moduleName: 'GL SW-Engineering',
-            from: '09:00',
-            to: '12:30',
-            room: 'HOR-120'
-        }
-    ],
-    montag: [
-        {
-            moduleName: 'Statistik',
-            from: '13:45',
-            to: '17:15',
-            room: 'HOR-120'
-        }
-    ]
-},18, 2022)
-    spC.renderCanvas();
-    spC.saveImage('./stundenplan.png');
 function loadConfig(): Config | undefined {
     return {
         discord:{
