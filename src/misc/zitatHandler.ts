669d75b1f49a14e671cb64a619fa18383e3a612f
Raqhael <Raphael.sack@gmx.de>
2022-10-04 10:21:53 +0200

    untested  new cite

--- src/misc/zitatHandler.ts
+++ src/misc/zitatHandler.ts
@@ -1,5 +1,6 @@
-import { client, config } from '../Bot';
+import { client, config, dualisInterface } from '../Bot';
 import { CacheType, ContextMenuInteraction, ModalSubmitInteraction, TextChannel, MessageEmbed } from 'discord.js';
+import { ScheduleWeek } from 'types/dualis';
 interface zitatHandler {
 
     zitatSenden: (interaction: ModalSubmitInteraction, name?: string) => void;
@@ -27,16 +28,50 @@ export default class ZitatHandler implements zitatHandler{
         name = name || this.user;
         let zitateChannel = await client.channels.fetch(config!.discord.zitate_channel) as TextChannel;
 
-        const msg = await zitateChannel.send(`${this.content} - ${name}`);
-        
-        const embed = new MessageEmbed()
-        .setTitle("Neues Zitat")
-        .setURL(msg.url)
-        .setDescription(`${this.content} - ${name}`)
-        .setTimestamp()
-        .setFooter({ text: "Gespeichert von: " +interaction.user.username, iconURL: interaction.user.avatarURL()!});
+            dualisInterface.getSchedule().catch(async (err) => {
+            console.error("Fehler beim Studneplan aufrufen in der Zitaterstellung " + err );
+            const msg = await zitateChannel.send(`${this.content} - ${name}`);
+            const embed = new MessageEmbed()
+            .setTitle("Neues Zitat")
+            .setURL(msg.url)
+            .setDescription(`${this.content} - ${name}`)
+            .setTimestamp()
+            .setFooter({ text: "Gespeichert von: " +interaction.user.username, iconURL: interaction.user.avatarURL()!});
+    
+            interaction.reply({ embeds: [embed] });
+        }).then(async (schedule) => {
+            // Get current weekday
+            let weekday = new Date().getDay();
+            let day = (["sonntag","montag","dienstag","mittwoch","donnerstag","freitag","samstag"] as Array<keyof ScheduleWeek>)[weekday];
+            let daySched = schedule?.schedule[day];
+            let currentLesson = daySched?.find((lesson) => {
+                return new Date(lesson.from).getHours() <= new Date().getHours() && new Date(lesson.to).getHours() >= new Date().getHours();
+            });
+            let lessonName = currentLesson?.moduleName;
 
-        interaction.reply({ embeds: [embed] });
+            if(lessonName){
+                const msg = await zitateChannel.send(`${this.content} - ${name} (Währned der ${lessonName} Vorlesung`);
+                const embed = new MessageEmbed()
+                .setTitle("Neues Zitat")
+                .setURL(msg.url)
+                .setDescription(`${this.content} - ${name}`)
+                .setTimestamp()
+                .setFooter({ text: "Gespeichert von: " +interaction.user.username, iconURL: interaction.user.avatarURL()!});
+    
+                interaction.reply({ embeds: [embed] });
+            }else{
+                const msg = await zitateChannel.send(`${this.content} - ${name}`);
+                const embed = new MessageEmbed()
+                .setTitle("Neues Zitat")
+                .setURL(msg.url)
+                .setDescription(`${this.content} - ${name}`)
+                .setTimestamp()
+                .setFooter({ text: "Gespeichert von: " + interaction.user.username, iconURL: interaction.user.avatarURL()! });
+                interaction.reply({ embeds: [embed] });
+            }
+            
+        });
+        
     }
 
 
