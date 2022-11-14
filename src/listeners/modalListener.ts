c1b1cadc6e041745d464dfe983f685cd945c4eaf
Raqhael <Raphael.sack@gmx.de>
2022-11-14 01:26:26 +0100

    zitat not crash

--- src/listeners/modalListener.ts
+++ src/listeners/modalListener.ts
@@ -8,7 +8,10 @@ export default async function modalListener(client: Client, interaction: ModalSu
         //Static ids
         default:
             if (interaction.customId.startsWith("zitatModal_")){
-                zitateMap[interaction.customId.split("_")[1]].zitatSenden(interaction,interaction.fields.getTextInputValue("name"))
+                const zitat = zitateMap[interaction.customId.split("_")[1]];
+                if (zitat){
+                   zitat.zitatSenden(interaction,interaction.fields.getTextInputValue("name")).catch(err => console.error("Could not send Zitat!", err));
+                }
                 delete zitateMap[interaction.customId.split("_")[1]];
             } else if (interaction.customId.startsWith("verifyModal_")){
                 verifyMap[interaction.customId.split("_")[1]].check(interaction,interaction.fields.getTextInputValue("id"))
