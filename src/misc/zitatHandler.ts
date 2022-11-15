339fa0a83572cb94f49013f338fed008ce7e94c6
Raqhael <Raphael.sack@gmx.de>
2022-11-15 09:52:44 +0100

    zitat embed

--- src/misc/zitatHandler.ts
+++ src/misc/zitatHandler.ts
@@ -1,5 +1,5 @@
 import { client, config } from '../Bot';
-import { CacheType, ContextMenuInteraction, ModalSubmitInteraction, TextChannel, MessageEmbed, User } from 'discord.js';
+import { CacheType, ContextMenuInteraction, ModalSubmitInteraction, TextChannel, MessageEmbed, User, MessageAttachment, Collection } from 'discord.js';
 import { ScheduleWeek } from 'types/schedule';
 interface zitatHandler {
 
@@ -12,26 +12,28 @@ interface zitatHandler {
     contextLink: string; // Link to the message
 }
 
-export default class ZitatHandler implements zitatHandler{
+export default class ZitatHandler implements zitatHandler {
     interaction: ContextMenuInteraction;
+    attachment: Collection<string, MessageAttachment>;
     content: string;
     user: User;
     id: string;
     contextLink: string;
 
-    constructor(interaction: ContextMenuInteraction, contextLink: string){
+    constructor(interaction: ContextMenuInteraction, contextLink: string) {
         this.interaction = interaction;
         this.contextLink = contextLink;
         this.content = interaction.options.get("message")!.message!.content;
+        this.attachment = interaction.options.get("message")!.message!.attachments as Collection<string, MessageAttachment>;
         this.user = interaction.options.get("message")!.message!.author as User;
         this.id = interaction.options.get("message")!.message!.id;
     }
 
-    zitatSenden = async (interaction: ModalSubmitInteraction,zitatAuthor?: string)  => {
+    zitatSenden = async (interaction: ModalSubmitInteraction, zitatAuthor?: string) => {
         zitatAuthor = zitatAuthor || this.user.username;
         let member = await interaction.guild?.members.fetch(this.user.id);
         if (member === null) {
-            console.error("Member not found");  
+            console.error("Member not found");
             return;
         }
 
@@ -39,18 +41,32 @@ export default class ZitatHandler implements zitatHandler{
         zitatAuthor = member?.nickname || member?.user.username || zitatAuthor;
         // get channel zitate from the guild
         const guild = interaction.guild;
-        if(guild === null){
+        if (guild === null) {
             console.error("Guild not found");
             return;
         }
         let zitateChannel = guild.channels!.cache.find(channel => channel.name === "zitate") as TextChannel;
-        if(zitateChannel === undefined){
+        if (zitateChannel === undefined) {
             console.error("Channel not found");
-            const msg = await interaction.reply({content: "ein Zitate-channel wurde nicht gefunden, du musst zunächst einen Channel mit dem Namen \"zitate\" auf deinem Discord anlegen", ephemeral: true});
+            const msg = await interaction.reply({ content: "ein Zitate-channel wurde nicht gefunden, du musst zunächst einen Channel mit dem Namen \"zitate\" auf deinem Discord anlegen", ephemeral: true });
             return;
         }
 
-        const msg = await zitateChannel.send(`${this.content} - ${zitatAuthor} ${this.contextLink}`);
+        const zitatEmbed = new MessageEmbed()
+            .setColor(member?.displayColor || "#000000")
+            .setTitle(zitatAuthor)
+            .setURL(this.contextLink)
+        
+        if (this.content !== "") {
+            zitatEmbed.setDescription(this.content)
+        }
+
+        if (this.attachment.size > 0) {
+            zitatEmbed.setImage(this.attachment.first()?.url || "");
+        }
+
+
+        const msg = await zitateChannel.send({ embeds: [zitatEmbed] });
         let embed = new MessageEmbed()
             .setTitle("Neues Zitat")
             .setURL(msg.url)
@@ -59,7 +75,7 @@ export default class ZitatHandler implements zitatHandler{
             .setFooter({ text: "Gespeichert von: " + zitatSaver, iconURL: interaction.user.avatarURL()! });
 
         await interaction.reply({ embeds: [embed] });
-        
+
     }
 
 
