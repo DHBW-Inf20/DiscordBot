573350304b3ad165e2c4c03914f08afdc59c8abb
Raqhael <Raphael.sack@gmx.de>
2022-12-09 02:27:57 +0100

    01 top p

--- src/commands/Ask.ts
+++ src/commands/Ask.ts
@@ -36,9 +36,9 @@ export const Ask: ContextMenuCommand = {
         let response = openAi.createCompletion({
             "model": "text-davinci-003",
             "prompt": `${customPrompt ? msg.content.substring(1) : `Horby ist ein zynischer Chatbot der den Fragesteller verspottet: \n Frage: ${msg.content} \n Horby: `}\n}`,
-            "temperature": 0.5,
+            "temperature": 0.0,
             "max_tokens": 100,
-            "top_p": 1,
+            "top_p": 0.1,
             "frequency_penalty": 0,
             "presence_penalty": 0
         }).then(res => {
