15e9fa32962421ffb82f4ffee0548d479d26bfec
Raqhael <Raphael.sack@gmx.de>
2023-03-08 10:25:20 +0100

    chatgpt

--- src/commands/Ask.ts
+++ src/commands/Ask.ts
@@ -8,6 +8,8 @@ import { ContextMenuCommand } from "../types/command";
 import { Configuration, OpenAIApi } from 'openai';
 import dba, { IDavinciData } from '../misc/databaseAdapter';
 
+let chatHistory: string[] = [];
+
 export const Ask: ContextMenuCommand = {
     name: "Horby fragen",
     type: "MESSAGE",
@@ -40,23 +42,45 @@ export const Ask: ContextMenuCommand = {
         }
         let customPrompt = msg.content.startsWith('#');
 
-
-
+        
         const openAi = new OpenAIApi(openAiconfig);
-        let response = openAi.createCompletion({
-            "model": "text-davinci-003",
-            "prompt": `${customPrompt ? msg.content.substring(1) : `${data?.personality_description} ${msg.content} \n Horby: `}\n}`,
-            "temperature": data?.temperature,
-            "max_tokens": data?.max_tokens,
-            "top_p": data?.top_p,
-            "frequency_penalty": 0,
-            "presence_penalty": 0
-        }).then(res => {
+        chatHistory.push(msg.content);
+        let chatMessages = chatHistory.map((msg, index) => {
+            return {
+                "role": "user",
+                "content": msg
+            }
+        });
+        if (chatHistory.length > 5) {
+            chatHistory.shift();
+        }
+        let chatGPTResponse = openAi.createCompletion({
+            "model": "gpt-3.5-turbo",
+            "messages": [
+                {"role": "system", "content": "Du bist Dietmar Zende, ein Dozent an der DHBW Horb für das Fach Consulting. Du hast eine eigene Vertriebsfirma mit dem Namen Boss-Factory und beantwortest fragen über Consulting und dem technischen Vertrieb, du bist sehr von dir überzeugt."},
+                [...chatMessages]
+            ]
+        } as unknown as any).then(res => {
             interaction.followUp({ content: `\`\`\`${msg?.content}\`\`\`\n${res.data.choices[0].text}` });
         }).catch(err => {
             console.log(err);
             interaction.followUp({ content: "Fehler beim Generieren einer antwort" });
             return;
         })
+        // let response = openAi.createCompletion({
+        //     "model": "text-davinci-003",
+        //     "prompt": `${customPrompt ? msg.content.substring(1) : `${data?.personality_description} ${msg.content} \n Horby: `}\n}`,
+        //     "temperature": data?.temperature,
+        //     "max_tokens": data?.max_tokens,
+        //     "top_p": data?.top_p,
+        //     "frequency_penalty": 0,
+        //     "presence_penalty": 0
+        // }).then(res => {
+        //     interaction.followUp({ content: `\`\`\`${msg?.content}\`\`\`\n${res.data.choices[0].text}` });
+        // }).catch(err => {
+        //     console.log(err);
+        //     interaction.followUp({ content: "Fehler beim Generieren einer antwort" });
+        //     return;
+        // })
     }
 };
